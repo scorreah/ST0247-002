0ac4cd52f3404ba5f675b16fd361461209f209a3
scorreah <scorreah@eafit.edu.co>
2021-03-28 18:11:37 -0500

    Código Proyecto Entrega 2

--- proyecto/codigo/RuteoVehiculosElectricos.java
+++ proyecto/codigo/RuteoVehiculosElectricos.java
@@ -1,7 +1,7 @@
 
 /**
  *
- * @author ljpalaciom
+ * @author Simon Correa, David Gomez, ljpalaciom
  */
 import java.awt.Color;
 import java.io.BufferedReader;
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import javax.swing.JFrame;
 public class RuteoVehiculosElectricos {
+    ArrayList<ArrayList<Integer>> rutas;
+    int noRutas;
     int n, m, u, breaks;
     public static double r, speed, Tmax, Smax, st_customer, Q;
     Digraph mapa;
@@ -48,7 +50,7 @@ public class RuteoVehiculosElectricos {
             lector.readLine();
             lector.readLine();
             coordenadas = new ArrayList<Pair<Float, Float>>();
-            mapa = new DigraphAM(n);
+            mapa = new DigraphAM(m);
             for (int i = 0; i <= m; i++) {
                 linea = lector.readLine();
                 lineaPartida = linea.split(" ");
@@ -57,14 +59,14 @@ public class RuteoVehiculosElectricos {
             tipoEstacion = new short[u];
             for (int i = 0; i < u; i++) {
                 linea = lector.readLine();
-                lineaPartida = linea.split(" ");
-                coordenadas.add(new Pair(Float.parseFloat(lineaPartida[2]), Float.parseFloat(lineaPartida[3])));
-                tipoEstacion[i] = Short.parseShort(lineaPartida[5]);
+                //lineaPartida = linea.split(" ");
+                //coordenadas.add(new Pair(Float.parseFloat(lineaPartida[2]), Float.parseFloat(lineaPartida[3])));
+                //tipoEstacion[i] = Short.parseShort(lineaPartida[5]);
             }
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     double tiempo = (Math.sqrt(Math.pow(coordenadas.get(i).first - coordenadas.get(j).first,2)+ 
-                    Math.pow(coordenadas.get(i).second - coordenadas.get(j).second, 2)))/speed;
+                                Math.pow(coordenadas.get(i).second - coordenadas.get(j).second, 2)))/speed;
                     mapa.addArc(i, j, tiempo);
                 }
             }
@@ -90,10 +92,12 @@ public class RuteoVehiculosElectricos {
             System.out.println(ex);
         }
     }
+
     @Override
     public String toString() {
         return "RuteoVehiculosElectricos{" + "r=" + r + ", speed=" + speed + ", Tmax=" + Tmax + ", Smax=" + Smax + ", st_customer=" + st_customer + ", Q=" + Q + ", tiempoSolucion=" + tiempoSolucion + '}';
     }
+
     public void exportarPuntosCSV() {
         try {
             PrintStream escribirCoordenadas = new PrintStream(new File("ArchivosGenerados\\Coordenadas.csv"));
@@ -106,25 +110,39 @@ public class RuteoVehiculosElectricos {
             System.out.println(ex);
         }
     }
-    public void exportarRutasCSV(ArrayList<ArrayList<Integer>> rutas) {
-        try {
-            int numRuta = 0;
-            for (ArrayList<Integer> ruta : rutas) {
-                PrintStream escribirCoordenadas = new PrintStream(new File("ArchivosGenerados\\ruta" + numRuta + ".csv"));
-                escribirCoordenadas.println("X,Y");
-                for (Integer verticeActual : ruta) {
-                    escribirCoordenadas.println(coordenadas.get(verticeActual).first + "," + coordenadas.get(verticeActual).second);
-                }
-                escribirCoordenadas.close();
-                numRuta++;
-            }
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex);
+
+    /*public void exportarRutasCSV(ArrayList<ArrayList<Integer>> rutas) {
+    try {
+    int numRuta = 0;
+    for (ArrayList<Integer> ruta : rutas) {
+    PrintStream escribirCoordenadas = new PrintStream(new File("ArchivosGenerados\\ruta" + numRuta + ".csv"));
+    escribirCoordenadas.println("X,Y");
+    for (Integer verticeActual : ruta) {
+    escribirCoordenadas.println(coordenadas[verticeActual].first + "," + coordenadas[verticeActual].second);
+    }
+    escribirCoordenadas.close();
+    numRuta++;
+    }
+    } catch (FileNotFoundException ex) {
+    System.out.println(ex);
+    }
+    }*/
+    public void solucionar(boolean test) {
+
+        calculos solucion = new calculos();
+        solucion.ahorros(this.mapa, this.mapa.size());
+        solucion.calculadorRuta(this.mapa);
+        if (test) {
+            this.rutas = solucion.obtenerRuta(); 
+        } else {
+            this.rutas = solucion.rutas;
         }
+        this.rutas.trimToSize();
+        this.noRutas = this.rutas.size();
+
+        System.out.println();
     }
-    public void solucionar() {
-    
-    }
+
     /**
      * Este metodo es un test para verificar que la solucion es correcta. 
      * @param rutas Es un contenedor de rutas representadas por un arraylist de parejas donde el primer elemento indica el nodo
@@ -132,15 +150,83 @@ public class RuteoVehiculosElectricos {
      * @return Verdadero si el tiempo de solucion expresado concuerda y si la bateria nunca esta por debajo de 0.
      */
     public boolean comprobarSolucion( ArrayList<ArrayList<Pair<Integer, Integer>>> rutas){
-       return false;
+        return false;
+    }
+
+    public static String[][] test(){
+        File f = new File("../DataSets");
+        ArrayList<String> names = new ArrayList<>(Arrays.asList(f.list()));
+        String[][] analisis = new String[names.size()][5];
+        int cont = 0;
+        for(String file: names) {
+            System.gc();
+            Runtime runtime = Runtime.getRuntime();
+            long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+            RuteoVehiculosElectricos problema1 = new RuteoVehiculosElectricos("../DataSets/"+file);
+            analisis[cont][2] = ""+problema1.m;
+            long mejor, peor, prom;
+            mejor = Long.MAX_VALUE;
+            peor = 0;
+            prom = 0;
+            for (int i = 0; i < 100; i++) {
+                long ti = System.currentTimeMillis();
+                problema1.solucionar(true);
+                long tf = System.currentTimeMillis();
+                long total = tf - ti;
+                mejor = total < mejor ? total : mejor;
+                peor = total > peor ? total : peor;
+            }
+            problema1.solucionar(false);
+            analisis[cont][3] = "" + problema1.noRutas;
+            analisis[cont][4] = "" + problema1.tiempoTotalRutas();
+   
+            prom = (peor + mejor) /2;
+            System.gc();
+            long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+            long memoryUsed = ((memoryAfter - memoryBefore)/1024); //Memoria en KB
+            analisis[cont][0] = "" + memoryUsed;
+            analisis[cont][1] = "" + peor;
+            cont++;
+        }
+
+        return analisis;
     }
     
-   
+    public double tiempoTotalRutas() {
+        double tiempo = 0;
+        for (ArrayList<Integer> ruta: this.rutas) {
+            tiempo += this.mapa.getWeight(0, ruta.get(0));
+            for (int i = 0; i < ruta.size()-1; i++) {
+                tiempo += this.mapa.getWeight(ruta.get(i), ruta.get(i+1));
+            }
+            tiempo += this.mapa.getWeight(0, ruta.get(ruta.size()-1));
+        }
+        return tiempo;
+    }
+    public static void clearScreen() {  
+        System.out.printf("\033[H\033[2J");  
+        System.out.flush();  
+        System.out.println();
+    } 
+
+    private static void printResults(String[][] analisis) {
+        clearScreen();
+        for(int i = 0; i < analisis.length; i++ ) {
+            System.out.printf("%s KB, %s ms, %s clientes, %s camiones, %s tiempo total de todas las rutas\n",
+                analisis[i][0], analisis[i][1], analisis[i][2], analisis[i][3], analisis[i][4]);
+        }
+    }
+
     public static void main(String[] args) {
         File f = new File("../DataSets");
         ArrayList<String> names = new ArrayList<>(Arrays.asList(f.list()));
-        RuteoVehiculosElectricos problema1 = new RuteoVehiculosElectricos("../DataSets/dummy.txt");
-        problema1.solucionar();
-        problema1.exportarPuntosCSV();
+        System.gc();
+
+        String[][] analisis = test();
+        printResults(analisis);
+        System.out.println("Bien hecho");
+        //DibujarRuta bueno = new DibujarRuta(
+        //problema1.exportarPuntosCSV();
     }
+
 }
--- proyecto/codigo/calculos.java
+++ proyecto/codigo/calculos.java
@@ -1,18 +1,25 @@
+/**
+ * Write a description of class C here.
+ * 
+ * @author David Gomez, Simon Correa
+ * @version 28/03/2021
+ */
 import java.util.ArrayList;
 public class calculos{
     double[][] ahorro;
     int ultimo;
     int contador;
-    ArrayList<ArrayList<Integer>> rutas;
-
+    int[] limpiados;
+    public static ArrayList<ArrayList<Integer>> rutas = new ArrayList<ArrayList<Integer>>();
     public void ahorros(Digraph mapa, int size){
-        contador = size;
+        contador = size-1;
+        limpiados = new int[size];
+        limpiados[0] = 1;
         ahorro = new double[size][size];
         for(int i = 1; i<size; i++){
             double primero = mapa.getWeight(0,i)*2;
             ahorro[0][i]= primero;
         }
-
         for(int i = 1; i<size;i++){
             for(int j = 1; j<size; j++){
                 if(i==j){
@@ -22,11 +29,16 @@ public class calculos{
                 double suma = ahorro[0][i]+ahorro[0][j];
                 double demora = mapa.getWeight(0,i) +mapa.getWeight(0,j)+mapa.getWeight(i, j);
                 ahorro[i][j] = suma-demora;
+                ahorro[j][i] = suma-demora;
             }
         }
+        for(int i = 1; i<size; i++){
+            ahorro[0][i]= 0;
+            ahorro[i][0]= 0;
+        }
     }
-
     public void calculadorRuta(Digraph mapa){
+        if(contador <= 0)return;
         ArrayList<Integer> registro = new ArrayList<Integer>();
         double mayor = 0;
         int x = 0;
@@ -40,45 +52,49 @@ public class calculos{
                 }
             }
         }
-
-        if(contador == 0)return;
-
+        if(mayor == 0) return;
         while(true){  
             /*
             if(registro.contains(x)){
-                for(int i= 0; i<mapa.size; i++){
-                    ahorro[x][i]=0;
-                    ahorro[i][x]= 0;
-                }
+            for(int i= 0; i<mapa.size; i++){
+            ahorro[x][i]=0;
+            ahorro[i][x]= 0;
+            }
             }
             if(registro.contains(y)){
-                for(int i= 0; i<mapa.size; i++){
-                    ahorro[y][i]=0;
-                    ahorro[i][y]= 0;
-                }
+            for(int i= 0; i<mapa.size; i++){
+            ahorro[y][i]=0;
+            ahorro[i][y]= 0;
             }
-            */
-
+            }
+             */
             registro = DemoraCamino(registro, x, y);
             if(!PodemosAgregarlo(registro, mapa)){
                 if(ultimo == 1)registro.remove(0);
                 if(ultimo == 2)registro.remove(registro.size()-1);
                 rutas.add(registro);
-                registro.clear();
+                //registro.clear();
+                limpiarResto(registro, mapa.size);
                 break;
             }
-
+            limpiar(registro, mapa.size);
             ahorro[x][y]=0;
             ahorro[y][x]=0;
-            
             mayor= 0;
+            if(contador - registro.size()==0){
+                rutas.add(registro);
+                break;
+            }
             x = registro.get(0);
             y= registro.get(registro.size()-1);
-
-            for(int i = 0; i<mapa.size; i++){
+            int posx, posy;
+            posx = posy = 0;
+            for(int i = 1; i<mapa.size; i++){
+                if(limpiados[i] == 1){
+                    continue;
+                }
                 double temp = 0;
                 int pos = 0;
-
                 if(ahorro[x][i]>ahorro[y][i]){
                     temp = ahorro[x][i];
                     pos = x;
@@ -86,20 +102,40 @@ public class calculos{
                     temp = ahorro[y][i];
                     pos = y;
                 }
-
+                if (i == x || i == y) continue;
                 if(mayor < temp){
                     mayor = temp;
-                    x = i;
-                    y = pos;
+                    posx = i;
+                    posy = pos;
                 }    
             }
+            x = posx;
+            y = posy;
         }
         contador = contador - (rutas.get(rutas.size()-1)).size();
         calculadorRuta(mapa);
     }
-
-
-
+    public void limpiar (ArrayList<Integer> registro, int tam) {
+        if (registro.size() <= 2) return;
+        for (int i = 1; i <= registro.size()-2; i++) {
+            if(limpiados[registro.get(i)] == 1)continue;
+            limpiados[registro.get(i)] = 1;
+            for (int j = 0; j < tam; j++) {
+                ahorro[j][registro.get(i)] = 0;
+                ahorro[registro.get(i)][j] = 0;
+            }
+        }
+    }
+    public void limpiarResto (ArrayList<Integer> registro, int tam) {
+        for (int j = 0; j < tam; j++) {
+            ahorro[j][registro.get(0)] = 0;
+            ahorro[registro.get(0)][j] = 0;
+            ahorro[j][registro.get(registro.size()-1)] = 0;
+            ahorro[registro.get(registro.size()-1)][j] = 0;
+        }
+        limpiados[registro.get(0)] = 1;
+        limpiados[registro.get(registro.size()-1)] = 1;
+    }
     public boolean PodemosAgregarlo(ArrayList<Integer> registro, Digraph mapa){
         double tiempoMax = 10;
         double tiempoVisita = RuteoVehiculosElectricos.st_customer*registro.size();
@@ -111,35 +147,36 @@ public class calculos{
             if(i == 0){
                 tiempoMax = tiempoMax-(mapa.getWeight(0,registro.get(i)));
             }
-
             tiempoMax = tiempoMax-(mapa.getWeight(registro.get(i), registro.get(i+1)));
         }
-        return (tiempoMax-tiempoVisita)>=0;
+        return (tiempoMax-tiempoVisita)>=0; 
     }
-
     public ArrayList<Integer> DemoraCamino(ArrayList<Integer> registro, int x, int y){
-        if(registro.isEmpty()){
-            registro.add(x);
-            registro.add(y);
-            return registro;
+        ArrayList<Integer> registro1 = registro;
+        if(registro1.isEmpty()){
+            registro1.add(x);
+            registro1.add(y);
+            return registro1;
         }
-        
-        int tam = registro.size();
-
-        if(x==registro.get(0)){
-            registro.add(0, y);
+        int tam = registro1.size();
+        if(x==registro1.get(0)){
+            registro1.add(0, y);
             ultimo = 1;
-        }else if(y==registro.get(0)){
-            registro.add(0, x);
+        }else if(y==registro1.get(0)){
+            registro1.add(0, x);
             ultimo = 1;
-        }else if(registro.get(tam-1)==x){
-            registro.add(y);
+        }else if(registro1.get(tam-1)==x){
+            registro1.add(y);
             ultimo = 2;
-        }else  if(registro.get(tam-1)==y){
-            registro.add(x);
+        }else  if(registro1.get(tam-1)==y){
+            registro1.add(x);
             ultimo = 2;
         }
-        return registro;
-
+        return registro1;
+    }
+    public static ArrayList<ArrayList<Integer>> obtenerRuta() {
+        ArrayList<ArrayList<Integer>> solucion = rutas;
+        rutas.clear();
+        return solucion;
     }
 }
